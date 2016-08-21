fa799e6e894cb7d81c8d714e2f0c2a6220c14fb9
Foo Pang <foo.pang@gmail.com>
2016-08-21 20:19:11 +0800

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import {Router, RouterConfiguration} from 'aurelia-router';
+import {inject} from 'aurelia-framework';
+import {WebAPI} from './web-api';
+
+@inject(WebAPI)
+export class App {
+  router: Router;
+
+  constructor(public api: WebAPI) {}
+
+  configureRouter(config: RouterConfiguration, router: Router){
+    config.title = 'Contacts';
+    config.map([
+      { route: '',              moduleId: 'no-selection',   title: 'Select'},
+      { route: 'contacts/:id',  moduleId: 'contact-detail', name:'contacts' }
+    ]);
+
+    this.router = router;
+  }
+}
